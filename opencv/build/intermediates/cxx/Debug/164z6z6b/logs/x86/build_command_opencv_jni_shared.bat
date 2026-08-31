5c7062300bbd200006b091e1665198f77e91cdb8
MyNameIsYourNightmare <andrea.sibilla03@gmail.com>
2026-08-31 14:10:51 +0200

    Ignore generated build artifacts and large files

--- opencv/build/intermediates/cxx/Debug/164z6z6b/logs/x86/build_command_opencv_jni_shared.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\andsi\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Users\\andsi\\AndroidStudioProjects\\Linee_Langer\\opencv\\.cxx\\Debug\\164z6z6b\\x86" ^
-  opencv_jni_shared
